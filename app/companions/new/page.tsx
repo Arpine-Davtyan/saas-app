1cbf8bd70965db7be1958fc8891db3ec32c015c7
Arpine-Davtyan <arpinedavtyan5@gmail.com>
2026-05-10 12:11:44 +0400

    Styles

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -12,7 +12,7 @@ const NewCompanion = async () => {
   const canCreateCompanion = await newCompanionPermissions();
 
   return (
-    <main className="mid:w-2/3 justify-center items-center">
+    <main className="lg:w-1/3 mid:w-2/3 justify-center items-center">
       {canCreateCompanion ? (
         <article className="w-full gap-4 flex flex-col">
           <h1>Companion Builder</h1>
